e2f717eb201aa591f0020ed54bd45624a870d1c0
Tom Schippke <tom.schippke12@gmail.com>
2026-08-31 17:49:46 +0200

    Fix flashcard button build error

--- src/app/eleve/flashcards/page.tsx
+++ src/app/eleve/flashcards/page.tsx
@@ -118,7 +118,7 @@ export default function FlashcardsPage() {
               <ArrowLeft className="h-5 w-5 mr-2" /> Précédente
             </Button>
             <Button 
-              variant="default" 
+              variant="primary" 
               size="lg" 
               onClick={handleNext} 
               disabled={currentIndex === flashcards.length - 1}
