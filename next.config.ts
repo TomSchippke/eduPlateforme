3fb9b16a96bbd61a67966b765a8f3e31b6515b1f
Tom Schippke <tom.schippke12@gmail.com>
2026-09-09 23:22:45 +0200

    Correction

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   serverExternalPackages: ["pdfjs-dist", "pdf-parse"],
-  experimental: {
-    outputFileTracingIncludes: {
-      "/api/**/*": ["./node_modules/pdf-parse/**/*", "./node_modules/pdfjs-dist/**/*"],
-    },
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/pdf-parse/**/*", "./node_modules/pdfjs-dist/**/*"],
   },
 };
 
